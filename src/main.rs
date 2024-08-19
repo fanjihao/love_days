c099e95dbf26a1a1ff57d6fc322c7eeecd8a270a
fanjihao <1181190638@qq.com>
2024-08-19 16:32:00 +0800

    .env

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ use chrono::{Local, NaiveDate};
 use errors::CustomError;
 use reqwest::Client;
 use serde_json::Value;
-use utils::{BAIDU_AK, LOVE_DAYS, OFFCIAL_APP_ID, OFFCIAL_APP_SECRET, PUSH_ID, TEMPLATE_ID};
+use dotenv::dotenv;
+use std::env;
 
 mod errors;
-mod utils;
 
 async fn weather_handler() -> Result<Value, CustomError> {
     let client = Client::new();
-    let url = format!("https://api.map.baidu.com/weather/v1/?district_id=510107&data_type=all&ak={}", BAIDU_AK);
+    let url = format!("https://api.map.baidu.com/weather/v1/?district_id=510107&data_type=all&ak={}", &get_baidu_ak());
     let res = client.get(url).send().await?;
     println!("res {:?}", res);
     let response_text = res.text().await?;
@@ -23,13 +23,13 @@ async fn weather_handler() -> Result<Value, CustomError> {
     data.map_err(Into::into)
 }
 
-pub async fn fetch_set_access_token() -> Result<String, CustomError> {
+async fn fetch_set_access_token() -> Result<String, CustomError> {
     let body = reqwest::get(
         "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="
             .to_string()
-            + OFFCIAL_APP_ID
+            + &get_official_app_id()
             + "&secret="
-            + OFFCIAL_APP_SECRET,
+            + &get_official_app_secret(),
     )
     .await?
     .text()
@@ -60,13 +60,13 @@ println!("{:?}", token);
             let forecasts = value.get("result").unwrap().get("forecasts").unwrap();
             let location = value.get("result").unwrap().get("location").unwrap();
             
-            let given_date = NaiveDate::parse_from_str(LOVE_DAYS, "%Y-%m-%d")?;  
+            let given_date = NaiveDate::parse_from_str(&get_love_days(), "%Y-%m-%d")?;  
             let today = Local::now().naive_local().date();
             let days_diff = (today - given_date).num_days();
 
             let json_data = serde_json::json!({
-                "touser": PUSH_ID,
-                "template_id": TEMPLATE_ID,
+                "touser": get_push_id(),
+                "template_id": get_template_id(),
                 "url": "http://weixin.qq.com/download",
                 "topcolor":"#FF0000",
                 "data":{
@@ -119,6 +119,32 @@ println!("{:?}", token);
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     println!("Hello, world!");
+
     let _ = send_template().await;
+}
+
+fn get_official_app_id() -> String {
+    env::var("OFFICIAL_APP_ID").expect("OFFICIAL_APP_ID not set")
+}
+
+fn get_official_app_secret() -> String {
+    env::var("OFFICIAL_APP_SECRET").expect("OFFICIAL_APP_SECRET not set")
+}
+
+fn get_baidu_ak() -> String {
+    env::var("BAIDU_AK").expect("BAIDU_AK not set")
+}
+
+fn get_push_id() -> String {
+    env::var("PUSH_ID").expect("PUSH_ID not set")
+}
+
+fn get_template_id() -> String {
+    env::var("TEMPLATE_ID").expect("TEMPLATE_ID not set")
+}
+
+fn get_love_days() -> String {
+    env::var("LOVE_DAYS").expect("LOVE_DAYS not set")
 }
