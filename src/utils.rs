060773f7402bfc3d7db752f238f0375a93f359e6
fanjihao <64520838+fanjihao@users.noreply.github.com>
2024-08-19 15:34:00 +0800

    Delete src/utils.rs

--- src/utils.rs
+++
@@ -1,6 +0,0 @@
-pub const OFFCIAL_APP_ID: &str = "wx640a2bd5fb33a287"; // 公众号测试
-pub const OFFCIAL_APP_SECRET: &str = "9a735dfa8561f510663a7e9e3c3a6a2e"; // 公众号测试
-pub const BAIDU_AK: &str = "HbPNlIT1eT06yPO3puiQuYrLhCt9zH2W";
-pub const PUSH_ID: &str = "ofmkB67yRAWPxjIKox2wxvHJALy8";
-pub const TEMPLATE_ID: &str = "-rnlOjKqvvuhIjKysIrTlzW0x-M_iryCNjQvLT58VuQ";
-pub const LOVE_DAYS: &str = "2022-03-01";
